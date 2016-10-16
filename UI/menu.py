b6b068f9dbcd1bf0717d198d5481d2d939d8e93c
ROADKILLSANTA <ericnie7@gmail.com>
2016-10-15 22:15:00 -0400

    Update menu.py

--- UI/menu.py
+++ UI/menu.py
@@ -30,11 +30,3 @@ def Load():
 	Slot4.grid(row=5,column=0)
 def options():
 	print("Undeveloped")
-def ingame():
-	root=tkinter.tk()
-	print("This is the Menu.")
-	Label(root,text="This is the Menu").grid(row=0,column=0)
-	Button(root,text="Resume Game", command=main.resume())
-	Button(root,text="Save Game",command=save())
-	Button(root,text="Load Game",command=load())
-	Button(root,text="Options",command=options())
