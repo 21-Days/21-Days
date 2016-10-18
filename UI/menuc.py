77a8d851bb423c331b605dab404ef810cb653e45
ROADKILLSANTA <ericnie7@gmail.com>
2016-10-17 21:12:23 -0400

    Update menuc.py

--- UI/menuc.py
+++ UI/menuc.py
@@ -1,19 +1,18 @@
 import main
 import menu
+import function
 from tkinter import *
 
 def Save():
+	function.clear()
 	root=tkinter.Tk()
 	labels=saveload.labels(False)
-	Slot1=Button(root, text=labels[1], command=saveload.save(1))
-	Slot1.grid(row=1,column=0)
-	Slot2=Button(root, text=labels[2], command=saveload.save(2))
-	Slot2.grid(row=2,column=0)
-	Slot3=Button(root, text=labels[3], command=saveload.save(3))
-	Slot3.grid(row=3,column=0)
-	Slot4=Button(root, text=labels[4], command=saveload.save(4))
-	Slot4.grid(row=4,column=0)
+	Button(root, text=labels[1], command=saveload.save(1)).grid(row=1,column=0)
+	Button(root, text=labels[2], command=saveload.save(2)).grid(row=2,column=0)
+	Button(root, text=labels[3], command=saveload.save(3)).grid(row=3,column=0)
+	Button(root, text=labels[4], command=saveload.save(4)).grid(row=4,column=0)
 def Load():
+	function.clear()
 	root=tkinter.Tk()
 	labels=saveload.labels(True)
 	Label(root, text=labels[0]).grid(row=0,column=0)
@@ -25,6 +24,7 @@ def Load():
 def options():
 	print("Undeveloped")
 def confirm():
+	function.clear()
 	root=tkinter.tk()
 	Label(root,text="Are you sure?").grid(row=0,column=0)
 	Button(root,text="Yes",command=menu.main()).grid(row=1,column=0)
