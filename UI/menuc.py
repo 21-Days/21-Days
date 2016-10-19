3f66d15be7626271fe1eb04f7524d1ab34b9637e
ROADKILLSANTA <ericnie7@gmail.com>
2016-10-18 20:09:51 -0400

    Update menuc.py

--- UI/menuc.py
+++ UI/menuc.py
@@ -1,5 +1,6 @@
 import gui
 import function
+import webbrowser
 from tkinter import *
 
 def Save(root,main):
@@ -9,7 +10,7 @@ def Save(root,main):
 	Button(root, text=labels[2], command=saveload.save(2)).grid(row=2,column=0)
 	Button(root, text=labels[3], command=saveload.save(3)).grid(row=3,column=0)
 	Button(root, text=labels[4], command=saveload.save(4)).grid(row=4,column=0)
-	if(main=True):
+	if(main==True):
 		Button(root, text="Back", command=gui.main()).grid(row=5,column=0)
 	else:
 		Button(root, text="Back", command=gui.ingame()).grid(row=5,column=0)
@@ -29,13 +30,32 @@ def Load(root,main):
 def options(root,main):
 	function.clear(root)
 	Label(root, text="Options", font=32).grid(row=0,column=0)
-	if(main=True):
+	if(main==True):
 		Button(root, text="Back", command=gui.main()).grid(row=6,column=0)
 	else:
 		Button(root, text="Back", command=gui.ingame()).grid(row=6,column=0)
-def confirm(root):
+def credits(root, main):
 	function.clear(root)
-	Label(root,text="Are you sure?").grid(row=0,column=0)
-	Button(root,text="Yes",command=gui.main()).grid(row=1,column=0)
-	Button(root,text="No",command=gui.ingame()).grid(row=2,column=0)
+	Label(root, text="Credits", font=32).grid(row=0,column=0)
+	Label(root, text="Programmer: Roadkillsanta (EN)", font=16).grid(row=1,column=0)
+	Label(root, text="Programmer: BOOMCHICKEN (JZ)", font=16).grid(row=4,column=0)
+	Label(root, text="Writer: GoobysTheName (WJ)", font=16).grid(row=3,column=0)
+	Label(root, text="Writer: BOOMCHICKEN (JZ)", font=16).grid(row=4,column=0)
+	Button(root, text="@RGTN (github.com/RGTN)", font=16, command=webbrowser.open("http:github.com/RGTN", new=2, autoraise=True)).grid(row=5,column=0)
+	Label(root, text="Special Thanks: GitHub (github.com)", font=16).grid(row=6,column=0)
+	Label(root, text="", font=16).grid(row=7,column=0)
+	if(main==True):
+		Button(root, text="Back", font=16, command=gui.main()).grid(row=8,column=0)
+	else:
+		Button(root, text="Back", font=16, command=gui.ingame()).grid(row=8,column=0)
+def confirm(root,main):
+	function.clear(root)
+	Label(root,text="Are you sure?").grid(row=0,column=0, columnspan=2)
+	if(main==True):
+		Button(root,text="Yes",command=gui.exit()).grid(row=1,column=0)
+		Button(root,text="No",command=gui.main()).grid(row=1,column=1)
+	else:
+		Button(root,text="Yes",command=gui.main()).grid(row=1,column=0)
+		Button(root,text="No",command=gui.ingame()).grid(row=1,column=1)
+	
 	
