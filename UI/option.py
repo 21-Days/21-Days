fdf948b41e64f621f0a5069aa58f91aee0c91005
ROADKILLSANTA <ericnie7@gmail.com>
2016-10-20 13:33:46 -0400

    Create option.py

---
+++ UI/option.py
@@ -0,0 +1,7 @@
+import main
+
+def choice(frame, choice1, choice2, choice3, choice4):
+	if(choice1 !=""):
+		Button(frame, text=choice1, command=).grid(row=0, column=0)
+	if(choice2 !=""):
+		Button(frame, text=choice2, command=).grid(row=1, column=0)
