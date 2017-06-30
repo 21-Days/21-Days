311fd3f32c0c2ab0df826a3060a18358dc3d9438
ROADKILLSANTA <ericnie7@gmail.com>
2017-06-30 18:39:14 -0400

    Update dependencies.js

--- skulpt/dependencies.js
+++ skulpt/dependencies.js
@@ -1,1 +1,16 @@
-
+Sk.externalLibraries = {
+	numpy : {
+		path: 'http://example.com/static/primeronoo/skulpt/external/numpy/__init__.js',
+		dependencies: ['/static/primeronoo/skulpt/external/deps/math.js'],
+	},
+	matplotlib : {
+		path: '/static/primeronoo/skulpt/external/matplotlib/__init__.js'
+	},
+	"matplotlib.pyplot" : {
+		path: '/static/primeronoo/skulpt/external/matplotlib/pyplot/__init__.js',
+		dependencies: ['/static/primeronoo/skulpt/external/deps/d3.min.js'],
+	},
+	"arduino": {
+		path: '/static/primeronoo/skulpt/external/arduino/__init__.js'
+	}
+};
